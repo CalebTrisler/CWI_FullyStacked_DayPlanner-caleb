fdb91178d755dd4689de07aea99cad12664d729d
socrastein <31764357+socrastein@users.noreply.github.com>
2026-03-04 14:43:45 -0700

    Setup menu button and outline of container items

--- modules/main.js
+++ modules/main.js
@@ -1,8 +1,14 @@
+import runTests from "../tests/runTests.js";
 import StorageManager from "./dataStorage.js";
 import appSettings from "./settings.js";
+import createMenu from "./settingsMenu.js";
 
 // Load user settings from localStorage when the application starts
 appSettings.loadSettings();
 
 // Load all saved calendar events from localStorage when the application starts
 const allEvents = StorageManager.loadAllEvents();
+
+createMenu();
+
+// runTests();
